da75dc74d30a597df90aa04eea8629174b000ab1
Vaibhav Ranglani <ranglani.vaibhav@gmail.com>
2016-05-24 16:26:06 +0530

    Added a commit for demo.

--- src/com/practice/javapractice/exceptions/CustomException.java
+++ src/com/practice/javapractice/exceptions/CustomException.java
@@ -3,6 +3,8 @@ package com.practice.javapractice.exceptions;
 /**
  * Created by vaibhavr on 14/04/16.
  */
+
+//This is a custom exception
 public class CustomException extends Exception {
 
     private String description;
