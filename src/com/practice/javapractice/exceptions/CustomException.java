7eee025da40cc8722d5c16ab31629aa0f85078ec
Vaibhav Ranglani <ranglani.vaibhav@gmail.com>
2016-05-24 16:29:17 +0530

    Adding commit for master

--- src/com/practice/javapractice/exceptions/CustomException.java
+++ src/com/practice/javapractice/exceptions/CustomException.java
@@ -3,6 +3,7 @@ package com.practice.javapractice.exceptions;
 /**
  * Created by vaibhavr on 14/04/16.
  */
+//Custom Class
 public class CustomException extends Exception {
 
     private String description;
